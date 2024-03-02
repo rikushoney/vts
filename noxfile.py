0779cc1500a6d44a9eedcaad14230dfef66cb663
Rikus Honey <rikushoney2@gmail.com>
2024-03-02 20:57:48 +0200

    update requirements in noxfile.py

--- noxfile.py
+++ noxfile.py
@@ -3,26 +3,26 @@ import nox
 
 @nox.session
 def tests(session: nox.Session):
-    session.install("maturin", "pytest")
+    session.install("-r", "requirements/tests.txt")
     session.run("maturin", "develop")
     session.run("pytest")
 
 
 @nox.session
 def check(session: nox.Session):
-    session.install("mypy", "ruff")
+    session.install("-r", "requirements/check.txt")
     session.run("ruff", "check", "python/vts")
     session.run("mypy", "python/vts")
 
 
 @nox.session
 def format(session: nox.Session):
-    session.install("black", "ruff")
+    session.install("-r", "requirements/format.txt")
     session.run("ruff", "check", "--select", "I", "python/vts")
     session.run("black", "--check", "python/vts")
 
 
 @nox.session
 def build(session):
-    session.install("maturin")
+    session.install("-r", "requirements/build.txt")
     session.run("maturin", "build")
