8b35ca985a2ab5c344a95462e0579927c5670949
Rikus Honey <rikushoney2@gmail.com>
2024-03-11 12:35:15 +0200

    add OpaqueKey trait

---
+++ crates/vts_shared/src/database.rs
@@ -0,0 +1,89 @@
+use std::marker::PhantomData;
+
+use crate::OpaqueKey;
+
+pub trait DbKey: OpaqueKey {}
+
+impl DbKey for u8 {}
+impl DbKey for u16 {}
+impl DbKey for u32 {}
+impl DbKey for u64 {}
+
+pub struct Database<T, I = u32> {
+    lookup_table: Vec<*const T>,
+    current: Vec<T>,
+    archived: Vec<Vec<T>>,
+    _unused: PhantomData<I>,
+}
+
+impl<T, I: DbKey> Database<T, I> {
+    pub fn with_capacity(capacity: usize) -> Self {
+        let lookup_table = Vec::new();
+        let current = Vec::with_capacity(capacity.next_power_of_two());
+        let archived = Vec::new();
+
+        Self {
+            lookup_table,
+            current,
+            archived,
+            _unused: PhantomData,
+        }
+    }
+
+    pub fn entry(&mut self, entity: T) -> I {
+        use std::mem;
+
+        assert!(self.lookup_table.len() <= I::max_index());
+
+        if self.current.len() == self.current.capacity() {
+            let new_capacity = self.current.capacity().next_power_of_two();
+            let new_storage = Vec::with_capacity(new_capacity);
+            let old_storage = mem::replace(&mut self.current, new_storage);
+            self.archived.push(old_storage);
+        }
+
+        self.current.push(entity);
+
+        let id = I::from_index(self.lookup_table.len());
+        let ptr = {
+            let index = self.current.len() - 1;
+            &self.current[index] as *const T
+        };
+        self.lookup_table.push(ptr);
+
+        id
+    }
+
+    pub fn lookup(&self, id: I) -> &T {
+        let id = id.as_index();
+        assert!(id < self.lookup_table.len());
+        let ptr = self.lookup_table[id];
+
+        // SAFETY: `ptr` is valid for the same lifetime as `self`.
+        unsafe { &*ptr }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_database() {
+        #[derive(Debug, PartialEq)]
+        struct Foo {
+            bar: usize,
+        }
+
+        let mut db = Database::<Foo>::with_capacity(1);
+        let test1 = db.entry(Foo { bar: 1 });
+        let test2 = db.entry(Foo { bar: 2 });
+        assert_eq!(db.lookup(test1), &Foo { bar: 1 });
+        assert_eq!(db.lookup(test2), &Foo { bar: 2 });
+
+        let test3 = db.entry(Foo { bar: 3 });
+        assert_eq!(db.lookup(test1), &Foo { bar: 1 });
+        assert_eq!(db.lookup(test2), &Foo { bar: 2 });
+        assert_eq!(db.lookup(test3), &Foo { bar: 3 });
+    }
+}
--- crates/vts_shared/src/lib.rs
+++ crates/vts_shared/src/lib.rs
@@ -1,2 +1,34 @@
 pub mod database;
 pub mod symboltable;
+
+pub trait OpaqueKey {
+    fn as_index(&self) -> usize;
+
+    fn from_index(idx: usize) -> Self;
+
+    fn max_index() -> usize;
+}
+
+macro_rules! impl_opaquekey {
+    ($ty:path, $max:path) => {
+        impl OpaqueKey for $ty {
+            fn from_index(idx: usize) -> Self {
+                assert!(idx < Self::max_index());
+                idx as $ty
+            }
+
+            fn as_index(&self) -> usize {
+                *self as usize
+            }
+
+            fn max_index() -> usize {
+                $max as usize
+            }
+        }
+    };
+}
+
+impl_opaquekey!(u8, u8::MAX);
+impl_opaquekey!(u16, u16::MAX);
+impl_opaquekey!(u32, u32::MAX);
+impl_opaquekey!(u64, u64::MAX);
