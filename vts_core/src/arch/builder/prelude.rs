486b69eb0165bf706cc73f7ef562f058b49c0ab1
Rikus Honey <rikushoney2@gmail.com>
2024-04-26 03:18:15 +0200

    add arch builder prelude

---
+++ vts_core/src/arch/builder/prelude.rs
@@ -0,0 +1,7 @@
+pub use super::super::{
+    component::{ComponentBuilder, ComponentBuilderNew, ComponentClass},
+    connection::{ConnectionBuilder, ConnectionBuilderNew},
+    module::{ComponentId, ComponentRefId, ConnectionId, PortId},
+    port::{PortBuilder, PortBuilderNew},
+    reference::{ComponentRefBuilder, ComponentRefBuilderNew},
+};
